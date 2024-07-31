3e2f562f3351da872bcc284d4ae15e4503854013
NikhilRNambiar <nikhilrnambiar007@gmail.com>
2024-07-31 18:33:18 +0530

    seconf commit

---
+++ src/Stopwatch.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+function Stopwatch() {
+    const [time,setTime]=useState(0);
+    const[isRunning,setIsRunning]=useState(false);
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+    };
+
+    
+
+    useEffect(()=>{
+        let interval;
+        if(isRunning){
+           interval= setInterval(()=>{
+                setTime((prev)=>prev+1)
+            },1000);
+        }
+        else{
+            clearInterval(interval);
+        }
+        return(()=>{
+            clearInterval(interval);
+        })
+    },[isRunning])
+  return (
+    <div>
+        <h1>Stopwatch</h1>
+        <h3>Time:{formatTime(time)}</h3>
+        <button onClick={()=>setIsRunning((prev)=>!prev)}>{isRunning?"Stop":"Start"}</button>
+        
+        <button onClick={()=>{setTime(0); setIsRunning(false);}}>Reset</button>
+    </div>
+  )
+}
+
+export default Stopwatch
